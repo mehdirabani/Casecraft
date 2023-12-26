d8ef162ccbc0bf2739c631e6eeb7ff6ec9502d0a
Mydiam <pgm.mehdirabani@gmail.com>
2023-12-26 12:41:31 +0330

    Responsiveness Menu

--- site/script/app.js
+++ site/script/app.js
@@ -1,5 +1,8 @@
 const navToggle = document.querySelector(".nav__toggle");
+const menoMobile =document.querySelector(".menu")
+
 
 navToggle.addEventListener("click", function () {
   navToggle.classList.toggle("nav__toggle--open");
+  menoMobile.classList.toggle("menu--open")
 });
